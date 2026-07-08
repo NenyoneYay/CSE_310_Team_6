639b3d5687e26842b6e284c43d76b9feda5a1247
J-Tibbs <tib22001@byui.edu>
2026-07-08 15:21:39 -0600

    Worked on giving style change page functionality

--- Backend/Server/static/styleChange.js
+++ Backend/Server/static/styleChange.js
@@ -1,14 +1,23 @@
 // Plans are to create an override css sheet on the super. Changes will be made to this file, so the revert button can then just clear out the override
-
+const bg_color_field = document.getElementById("primary_bg_color_field"); 
 
 document.getElementById("commit_button").addEventListener("click", () =>{
+    var color = bg_color_field.value;
     event.preventDefault();
-    var color = document.getElementById("primary_bg_color_field").value;
-    console.log(color);
-    if(color.startsWith("#")){
-        document.body.style.backgroundColor = color;
+    if (color != ""){
+        if(color.startsWith("#")){
+            document.documentElement.style.setProperty('--primary-color', color);
+        }
+        else{
+            document.documentElement.style.setProperty('--primary-color', '#' + color);
+        }
+
     }
-    else{
-    document.body.style.backgroundColor = "#" + color;
-    }
+});
+
+document.getElementById("revert_button").addEventListener("click", () =>{
+    event.preventDefault();
+    document.documentElement.style.removeProperty('--primary-color');
+    bg_color_field.value = '';
+
 });
