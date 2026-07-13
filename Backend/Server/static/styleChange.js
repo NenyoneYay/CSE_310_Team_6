a6e2e769f137511f116bf1e0d07f21fed91f6569
J-Tibbs <tib22001@byui.edu>
2026-07-13 15:00:38 -0600

    Continued on the change styles page

--- Backend/Server/static/styleChange.js
+++ Backend/Server/static/styleChange.js
@@ -5,28 +5,63 @@ const header_color_field = document.getElementById("header_bg_color_field");
 const word_color_field = document.getElementById("primary_font_color_field");
 // Fonts
 const header_font_field = document.getElementById("header_font_field");
-const text_font_field = document.getElementById("text_font_field");
+// const text_font_field = document.getElementById("text_font_field");
 //Scale
 const text_scale = document.getElementById("font_scale_field");
+
 document.getElementById("commit_button").addEventListener("click", () =>{
-    var bg_color = bg_color_field.value;
-    var word_color = word_color_field.value;
-    var hbg_color = header_color_field.value;
-    var hfont = header_font_field.value;
-    var tfont = text_font_field.value;
-    var scale = text_scale.value;
-    
     event.preventDefault();
-    if (color != ""){
-        if(color.startsWith("#")){
-            document.documentElement.style.setProperty('--primary-color', bg_color);
-            document.documentElement.style.setProperty('--parchment-color', word_color);
+    // var bg_color = bg_color_field.value;
+    // var word_color = word_color_field.value;
+    // var hbg_color = header_color_field.value;
+    // var hfont = header_font_field.value;
+    // var tfont = text_font_field.value;
+    // var scale = text_scale.value;
+    
+    const color_changes = [bg_color_field, header_color_field, word_color_field];
+    const font_changes = [ header_font_field];
+
+
+    for(const item of color_changes){
+        if (item.value != ""){ 
+            console.log(item);
+            if(item.value.startsWith("#")){
+                if(item == bg_color_field){
+                    document.documentElement.style.setProperty('--primary-color', item.value);
+                }
+                else if(item == header_color_field){
+                    document.documentElement.style.setProperty('--ivy', item.value);
+                }
+                else if(item == word_color_field){
+                    document.documentElement.style.setProperty('--parchment-color', item.value);
+                }
+                // document.documentElement.style.setProperty('--parchment-color', word_color);
+            }
+            else{
+                if(item == bg_color_field){
+                    document.documentElement.style.setProperty('--primary-color', '#' + item.value);
+                }
+                else if(item == header_color_field){
+                    document.documentElement.style.setProperty('--ivy','#' +  item.value);
+                }
+                else if(item == word_color_field){
+                    document.documentElement.style.setProperty('--parchment-color','#' +  item.value);
+                }
+                // document.documentElement.style.setProperty('--parchment-color', '#' + word_color);
+            }
+    
         }
+        
         else{
-            document.documentElement.style.setProperty('--primary-color', '#' + bg_color);
-            document.documentElement.style.setProperty('--parchment-color', '#' + word_color);
+            continue; 
         }
 
+    }
+    
+    for (const item of font_changes){
+        if (item.value != ''){
+            document.documentElement.style.setProperty('--heading-font', item.value);
+        }
     }
 });
 
@@ -34,6 +69,8 @@ document.getElementById("revert_button").addEventListener("click", () =>{
     event.preventDefault();
     document.documentElement.style.removeProperty('--primary-color');
     document.documentElement.style.removeProperty('--parchment-color');
+    document.documentElement.style.removeProperty('--ivy');
+    document.documentElement.style.removeProperty('--heading-font');
 
 });
 
@@ -41,6 +78,13 @@ document.getElementById("clear_button").addEventListener("click", ()=>{
     event.preventDefault();
     document.documentElement.style.removeProperty('--primary-color');
     document.documentElement.style.removeProperty('--parchment-color');
+    document.documentElement.style.removeProperty('--ivy');
+    document.documentElement.style.removeProperty('--heading-font');
+    
     bg_color_field.value = '';
+    header_color_field.value = '';
     word_color_field.value = '';
+    header_font_field.value = '';
+    // text_font_field.value = '';
+    text_scale.value = '';
 });
